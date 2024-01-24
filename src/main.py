db9aff847f49d36821f4620d361101955428b22a
Joseph Clyde Mar <clydemar234@gmail.com>
2024-01-24 23:48:16 +0800

    chore(main.py): Add initial code to open the pygame window

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+import pygame
+
+pygame.init()
 
 if __name__ == '__main__':
-    pass
+    SCREEN_WIDTH, SCREEN_HEIGHT = 1100, 600
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    run = False
+        screen.fill((0, 0, 0))
+        pygame.display.update()
